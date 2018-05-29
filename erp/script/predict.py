8d81a335e6d3e89628f1b9ee1e85e7b16ff098dd
mingzhili <mingzhi.li@ihandysoft.com>
2018-05-29 14:58:17 +0800

    update

--- erp/script/predict.py
+++ erp/script/predict.py
@@ -72,7 +72,7 @@ class linear(object):
         for i in range(len(x)):
             day = x[i][0]
             for t in time_full:
-                if str(day) == t[-2:]:
+                if int(day) == int(t[-2:]):
                     time = t
                     break
             # time = datetime.date.today().strftime("%Y-%m-{}".format(date))
